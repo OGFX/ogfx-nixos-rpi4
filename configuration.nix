02a6fc766918b7c25e0812f56821551c8be4953e
fps <mista.tapas@gmx.net>
2020-07-14 19:47:28 +0200

    clean up the config a little

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ fetchurl, config, lib, pkgs, stdenv, buildPackages, fetchFromGitHub, perl, buildLinux, rpiVersion, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   fileSystems = lib.mkForce {
@@ -13,136 +13,180 @@
     };
   };
 
-  hardware.enableRedistributableFirmware = true;
+  hardware = {
+    enableRedistributableFirmware = true;
+    bluetooth.enable = false;
+  };
 
-  boot.loader.grub.enable = false;
-  boot.loader.raspberryPi.enable = true;
-  boot.loader.raspberryPi.version = 4;
+  boot = {
+    loader = {
+      grub.enable = false;
+      raspberryPi.enable = true;
+      raspberryPi.version = 4;
+    };
 
-  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.callPackage ./kernel.nix { 
-    rpiVersion = 4;  
-    kernelPatches = with pkgs.kernelPatches; [
-      bridge_stp_helper
-      request_key_helper
-      { 
-        name = "4.19.94rt39";
-        patch = builtins.fetchurl "https://www.kernel.org/pub/linux/kernel/projects/rt/4.19/older/patch-4.19.94-rt39.patch.xz";
-        sha256 = "5vm7dpdaspwyccx2lh6sycfcaaiw1439fpnhypm5cya0ymsnz0fj";
-      }
-      {
-        name = "usb-lowlatency";
-        patch = ./usb-lowlatency.diff;
-        sha256 = "5vm7dpdaspwyccx2lh6sycfcaaiw1439fpnhypm5cya0ymsnz0fj";
-      }
-    ];
-    extraConfig = ''
-      PREEMPT_RT_FULL y
+    kernelPackages = pkgs.linuxPackagesFor (pkgs.callPackage ./kernel.nix {
+      rpiVersion = 4;
+      kernelPatches = with pkgs.kernelPatches; [
+        bridge_stp_helper
+        request_key_helper
+        {
+          name = "4.19.94rt39";
+          patch = builtins.fetchurl "https://www.kernel.org/pub/linux/kernel/projects/rt/4.19/older/patch-4.19.94-rt39.patch.xz";
+          sha256 = "5vm7dpdaspwyccx2lh6sycfcaaiw1439fpnhypm5cya0ymsnz0fj";
+        }
+        {
+          name = "usb-lowlatency";
+          patch = ./usb-lowlatency.diff;
+          sha256 = "5vm7dpdaspwyccx2lh6sycfcaaiw1439fpnhypm5cya0ymsnz0fj";
+        }
+      ];
+      extraConfig = ''
+        PREEMPT_RT_FULL y
+      '';
+    });
+  
+    extraModprobeConfig = ''
+      options snd-usb-audio max_packs=1 max_packs_hs=1 max_urbs=12 sync_urbs=4 max_queue=1
     '';
-  });
 
-  boot.extraModprobeConfig = ''
-    options snd-usb-audio max_packs=1 max_packs_hs=1 max_urbs=12 sync_urbs=4 max_queue=1
-  '';
-
-  boot.consoleLogLevel = lib.mkDefault 7;
+    consoleLogLevel = lib.mkDefault 7;
+    enableContainers = false;
+  };
+ 
+  powerManagement.cpuFreqGovernor = "performance";
 
   fonts.fontconfig.enable = false;
-
-  hardware.bluetooth.enable = false;
-
-  powerManagement.cpuFreqGovernor = "performance";
 
   programs.command-not-found.enable = false;
 
   security.polkit.enable = false;
 
-  environment.variables = {
-    LV2_PATH = "/nix/var/nix/profiles/system/sw/lib/lv2/";
+  networking = {
+    interfaces.wlan0.ipv4.addresses = [ { address = "192.168.150.1"; prefixLength = 24; } ];
+    interfaces.eth0.useDHCP = true;
+
+    hostName = "ogfx-dev";
+    useDHCP = false;
+    networkmanager.enable = true;
+
+    # Disable firewall to make dnsmasq and hostapd work.
+    # This needs fixing :)
+    firewall.enable = false;
+
+    localCommands = ''
+      echo 1 > /proc/sys/net/ipv4/ip_forward
+      ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE 
+      ${pkgs.iptables}/bin/iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT 
+      ${pkgs.iptables}/bin/iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+    '';
+
+    networkmanager.extraConfig = ''
+      [keyfile]
+      unmanaged-devices=interface-name:wlan0
+    '';
   };
-
-
-  services.dnsmasq.enable = true;
-  services.dnsmasq.extraConfig = ''
-    # Bind to only one interface
-    bind-interfaces
-
-    # Choose interface for binding
-    interface=wlan0
-
-    # Specify range of IP addresses for DHCP leasses
-    dhcp-range=192.168.150.100,192.168.150.200
-  '';
-
-  services.hostapd.enable = true;
-  services.hostapd.interface = "wlan0";
-  services.hostapd.ssid = "ogfx";
-  services.hostapd.wpaPassphrase = "omg ogfx";
-
-  networking.interfaces.wlan0.ipv4.addresses = [ { address = "192.168.150.1"; prefixLength = 24; } ];
-
-  networking.hostName = "ogfx";
-  networking.useDHCP = false;
-  networking.interfaces.eth0.useDHCP = true;
-  networking.networkmanager.enable = true;
-
-  # Disable firewall to make dnsmasq and hostapd work.
-  # This needs fixing :)
-  networking.firewall.enable = false;
-
-  networking.localCommands = ''
-    echo 1 > /proc/sys/net/ipv4/ip_forward
-    ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-    ${pkgs.iptables}/bin/iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-    ${pkgs.iptables}/bin/iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-  '';
-
-  networking.networkmanager.extraConfig = ''
-    [keyfile]
-    unmanaged-devices=interface-name:wlan0
-  '';
-
-  services.openssh.enable = true;
-  services.openssh.startWhenNeeded = true;
 
   i18n.defaultLocale = "en_US.UTF-8";
 
   time.timeZone = "Europe/Amsterdam";
 
-  environment.systemPackages = with pkgs; [
-    iptables links
-    raspberrypi-tools
-    vim nano stress
-    htop tmux git
-    gdb
+  sound.enable = true;
 
-    jalv lilv lv2
-    guitarix gxplugins-lv2 
-    swh_lv2 calf
-    mda_lv2 rkrlv2
-    mod-distortion  
+  services = {
+    dnsmasq = {
+      enable = true;
+      extraConfig = ''
+        # Bind to only one interface
+        bind-interfaces
+
+        # Choose interface for binding
+        interface=wlan0
+
+        # Specify range of IP addresses for DHCP leasses
+        dhcp-range=192.168.150.100,192.168.150.200
+      ''; 
+    };
+
+    hostapd = {
+      enable = true;
+      interface = "wlan0";
+      ssid = "ogfx-dev";
+      wpaPassphrase = "omg it's fx";
+    };
+
+    openssh = {
+      enable = true;
+      startWhenNeeded = true;
+    };
+
+    xserver.enable = false;
+
+    jack = {
+      jackd = {
+        enable = true;
+        extraOptions = [ "-R" "-P 80" "-d" "alsa" "-p" "128" "-n" "2" "-d" "hw:iXR" ];
+      };
+    };
+
+    cron = {
+      enable = true;
+      systemCronJobs = [
+        "02 6 * * *  root . /etc/profile; bash /etc/nixos/borg-backup.sh >> /tmp/borg-backup.log 2>&1"
+      ];
+    };
+  };
+
+  systemd.services.ogfx-frontend = {
+    enable = true;
+    description = "The OGFX web frontend";
+    wantedBy = [ "jack" ];
+    serviceConfig = {
+      Type = "exec";
+      User="ogfx";
+      ExecStart = "${pkgs.bash}/bin/bash -l -c \". /etc/profile; ${pkgs.jack2}/bin/jack_wait -w; ${pkgs.ogfx-ui}/bin/ogfx_frontend_server.py\"";
+      # PAMName="ogfx";
+      LimitRTPRIO = 99;
+      LimitMEMLOCK = "infinity";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    nano
+    htop
+    tmux
+    wirelesstools
+    usbutils
+    iptables
+    gdb
+    raspberrypi-tools
+
+    borgbackup 
+
+    jalv
+    lv2
+    lilv
+    
+    guitarix
+    gxplugins-lv2
+    swh_lv2
+    calf
+    mda_lv2
+    rkrlv2
+    mod-distortion
+    gxmatcheq-lv2
+    zam-plugins
+    fomp
+    infamousPlugins
 
     ogfx-tools
     ogfx-ui
   ];
+  #  eq10q bshapr 
 
-  # These packages would be nice to have, but they
-  # don't build on aarch64-linux yet:
-  # 
-  # mod-distortion mda_lv2 infamousPlugins
-  # gxmatcheq-lv2 eq10q bshapr fomp rkrlv2
-  # zam-plugins 
-
-  services.xserver.enable = false;
-
-  sound.enable = true;
-
-  # Change "hw:USB" to the name of the ALSA pcm device of your
-  # soundcard (see e.g. /proc/asound/cards)
-  services.jack = {
-    jackd = {
-      enable = true;
-      extraOptions = [ "-R" "-P 80" "-d" "alsa" "-d" "hw:USB" ];
-    };
+  environment.variables = { 
+    LV2_PATH = "/nix/var/nix/profiles/system/sw/lib/lv2/"; 
   };
 
   users.users.ogfx = {
